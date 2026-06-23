a00b42fb2f4b912c46c0e77626ee275c7932d4cd
Thikash Jayaprakash <193379437+TjWhiteboy@users.noreply.github.com>
2026-06-23 14:13:40 +0530

    Add files via upload

---
+++ Java/areaofcircle.java
@@ -0,0 +1,13 @@
+// To find the radius of the circle 
+
+import java.util.Scanner;
+
+public class areaofcircle{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double b=3.14;
+        double area=b*a*a;
+        System.out.println("The area of the circle"+area);
+    }
+}
